0f561d8a25d39b8b610b7e8c00f7f6a2e4bfd07e
formation <formation@PP-00045.(none)>
2014-03-17 16:42:44 +0100

    commit modif controler

--- src/Angie/BlogBundle/Controller/DefaultController.php
+++ src/Angie/BlogBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace Angie\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Template("AngieBlogBundle:Default:index.html.twig")
+     */
     public function indexAction($name)
     {
-        return $this->render('AngieBlogBundle:Default:index.html.twig', array('name' => $name));
+	//var_dump($this->getUser());exit();
+        //return $this->render('AngieBlogBundle:Default:index.html.twig', array('name' => $name));
+	return array('name' => $name);
     }
 }
